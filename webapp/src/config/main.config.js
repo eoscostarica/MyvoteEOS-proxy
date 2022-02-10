ec6bf6675768292e53cb39a355491ffc8add8878
Xavier Fernandez <xavier@crservers.com>
2022-02-10 15:17:44 -0600

    fix(webapp): Apply latest name changes

--- webapp/src/config/main.config.js
+++ webapp/src/config/main.config.js
@@ -1,3 +1,3 @@
-export const myvoteeosdaoAccount =
+export const myvoteeosAccount =
   process.env.REACT_APP_MYVOTEEOS_ACCOUNT || 'myvoteeosdao'
 export const blockExplorer = process.env.REACT_APP_BLOCK_EXPLORER_URL
